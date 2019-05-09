b4f2df2a2127cc69aca82e1cb8fed2bced822aa6
MuneebDevs <muneebjavamob@gmail.com>
2019-05-09 12:45:14 +0500

    dry and apply policies

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,22 +1,22 @@
 class ProjectsController < ApplicationController
-  
+  before_action :authenticate_user!
   before_action :find_project, only: [:show, :edit, :update, :destroy] 
-  before_action :varify_creator, only: [:show, :edit, :update, :destroy]
+  # before_action :varify_creator, only: [:show, :edit, :update, :destroy]
+  before_action :project_authorize, only: [:show, :edit, :update, :destroy]
   
   def index
-    if(current_user.user_type == 'manager')
-  	  @projects = Project.where(created_by:current_user.id)
-    elsif(current_user.user_type == 'qa')
-      @projects = Project.all
-    end
+    @projects = policy_scope(Project)
+    authorize @projects
   end
   
   def new
   	@project = Project.new
+    authorize @project
   end
   
   def create
     @project = current_user.projects.new(project_params)
+    authorize @project
     @project.created_by = current_user.id
     if @project.save
       redirect_to @project
@@ -40,17 +40,16 @@ class ProjectsController < ApplicationController
   end
 
   def destroy
-    @project.destroy
     redirect_to projects_path
   end  
 
   private 
 
-  def varify_creator
-    if !(@project.created_by == current_user.id)
-        redirect_to projects_path, notice: "Record not found" 
-    end 
-  end
+  # def varify_creator
+  #   if !(@project.created_by == current_user.id)
+  #       redirect_to projects_path, notice: "Record not found" 
+  #   end 
+  # end
   def find_project
     @project = Project.find(params[:id])
   end
@@ -59,5 +58,8 @@ class ProjectsController < ApplicationController
     params.require(:project).permit(:name,:description)
   end
 
+  def project_authorize
+    authorize @project
+  end
 
 end
