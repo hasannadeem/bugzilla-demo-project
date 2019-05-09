1c036c0b55e4133e34f2cfb6d83b4e501d991edb
MuneebDevs <muneebjavamob@gmail.com>
2019-05-09 17:49:57 +0500

    add uniqueness to bug title as per project

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,7 +2,7 @@ class Bug < ApplicationRecord
 	belongs_to :creator, :class_name => "User"
 	belongs_to :project
 
-	validates :title, uniqueness: true
+	validates_uniqueness_of :title, scope: :project_id
 
 	mount_uploader :screen_shot, ImageUploader
 end
