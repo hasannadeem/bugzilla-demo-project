1bd8489f0752422b95c1b10664054ef7db5fcb50
MuneebDevs <muneebjavamob@gmail.com>
2019-05-09 15:06:34 +0500

    dry

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,24 +1,7 @@
 class HomeController < ApplicationController
 	def index
 	  if current_user
-		if current_user.user_type == 'manager'
-			redirect_to manager_url
-		elsif current_user.user_type == 'developer'
-			redirect_to developer_url
-		elsif current_user.user_type == 'qa'
-		 	redirect_to qa_url 
-		end
+	  	redirect_to projects_url
 	  end		
-	end
-	def manager
-		redirect_to projects_url
-	end
-	
-	def developer
-		
-	end
-
-	def qa
-		redirect_to projects_url
 	end
 end
