e8b91aeae30c668e21dcab077cea30b29b764a6f
MuneebDevs <muneebjavamob@gmail.com>
2019-05-09 12:43:58 +0500

    include pundit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,23 @@
 class ApplicationController < ActionController::Base
 
- before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  
+  include Pundit
+
+   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name,:user_type])
+  end
+
+  private
+
+  def user_not_authorized
+    flash[:alert] = "You are not authorized to perform this action."
+    redirect_to(request.referrer || root_path)
   end
 
 end
