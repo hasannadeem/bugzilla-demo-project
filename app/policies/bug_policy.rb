0af40414c06c6a0d92250792f43c35845d79dd57
MuneebDevs <muneebjavamob@gmail.com>
2019-05-10 19:20:25 +0500

    add policies

--- app/policies/bug_policy.rb
+++ app/policies/bug_policy.rb
@@ -3,6 +3,23 @@ class BugPolicy < ApplicationPolicy
   def index?
     @user.developer?
   end
+  
+  def create?
+  	@user.qa?
+  end
+
+  def assign?
+  	@user.user_type=='developer'
+  end 
+
+  def start_working?
+  	@user.id == @record.assign_to
+  end 
+
+  def work_done?
+  	@user.id == @record.assign_to
+  end 
+
   class Scope < Scope
     def resolve
       scope.all
