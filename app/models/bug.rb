143e1f902fcb1715a17bf4c7765a29e7e4174fce
MuneebDevs <muneebjavamob@gmail.com>
2019-05-10 19:16:50 +0500

    add validations

--- app/models/bug.rb
+++ app/models/bug.rb
@@ -2,7 +2,16 @@ class Bug < ApplicationRecord
 	belongs_to :creator, :class_name => "User"
 	belongs_to :project
 
+	validates :title, :bug_type, :status, presence: true
 	validates_uniqueness_of :title, scope: :project_id
-
+	validate :screen_shot_type
 	mount_uploader :screen_shot, ImageUploader
+
+	private
+
+	def screen_shot_type
+   	  if !screen_shot.content_type.to_s.in?(%('image/png image/gif')) 
+   	    errors.add(:screen_shot, "only support .png or .gif")  
+   	  end
+	end
 end
