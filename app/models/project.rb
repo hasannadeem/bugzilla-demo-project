8f37255a04d5e9d3d054fd9000a6cc9913dec8fa
MuneebDevs <muneebjavamob@gmail.com>
2019-05-13 12:20:19 +0500

    apply name validation

--- app/models/project.rb
+++ app/models/project.rb
@@ -2,6 +2,6 @@ class Project < ApplicationRecord
   has_many :project_users
   has_many :users, through: :project_users , dependent: :destroy
   has_many :bugs , dependent: :destroy
-
+  
   validates :name , presence: true
 end
