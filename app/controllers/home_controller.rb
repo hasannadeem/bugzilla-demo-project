7ed41a0e7b5ee0d196738e5f52b663ecb27f8053
MuneebDevs <muneebjavamob@gmail.com>
2019-05-14 16:04:48 +0500

    clean code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,8 @@
 class HomeController < ApplicationController
+	
 	skip_before_action :authenticate_user!
+
 	def index
-	  if current_user
-	  	redirect_to projects_url
-	  end		
+	  	redirect_to projects_url if current_user
 	end
 end
--- app/controllers/project_users_controller.rb
+++ app/controllers/project_users_controller.rb
@@ -1,18 +1,18 @@
 class ProjectUsersController < ApplicationController
-layout false
-before_action :find_project_user, only: [:destroy]
+  
+  layout false
+
+  before_action :find_project_user, only: [:destroy]
 
   def create
     project_user = ProjectUser.new(project_id:params[:project_id],user_id:params[:user_id])
     
     if project_user.save!
-    	return :json => '{msg: "user added"}'
+    	render :json => {:msg => "user added"}
     else
-    	return :json => '{msg: "user not added"}'
+    	render :json => {:msg => "user not added"}
     end
-
   end
-
 
   def destroy
     @project_users.each do |project_user|
@@ -24,10 +24,11 @@ before_action :find_project_user, only: [:destroy]
   private
 
   def find_project_user
-    @project_users = ProjectUser.where(user_id:params[:user_id],project_id:params[:project_id])
+    @project_users ||= ProjectUser.where(user_id:params[:user_id],project_id:params[:project_id])
   end
 
   def project_params
     params.require(:project_user).permit(:project_id,:user_id)
   end
+
 end
