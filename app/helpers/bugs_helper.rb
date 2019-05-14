67b6cfd275ec619e710a4b4e11ca458bbf9a4a9b
MuneebDevs <muneebjavamob@gmail.com>
2019-05-14 14:35:24 +0500

    create helper

--- app/helpers/bugs_helper.rb
+++ app/helpers/bugs_helper.rb
@@ -1,6 +1,19 @@
 module BugsHelper
-	def data_for_view(features, bugs)
-		@view_data = {:features => features, :bugs => bugs}
-		# @view_data < {:feature_count => features.count}
+	def data_for_view(all_bugs)
+		bugs = all_bugs.where(bug_type:'bug')
+		features = all_bugs.where(bug_type:'feature')
+		
+		view_data = {:features => features, :bugs => bugs}
+		
+		view_data.merge! total_features:features.count
+		view_data.merge! new_features:features.where(status:'new').count
+		view_data.merge! started_features:features.where(status:'started').count
+		view_data.merge! completed_features:features.where(status:'completed').count
+
+		view_data.merge! total_bugs:bugs.count
+		view_data.merge! new_bugs:bugs.where(status:'new').count
+		view_data.merge! started_bugs:bugs.where(status:'started').count
+		view_data.merge! resolved_bugs:bugs.where(status:'resolved').count
+
 	end
 end
