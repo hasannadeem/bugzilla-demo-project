5ad0034015a3c699af575403d32a9778bf931da7
MuneebDevs <muneebjavamob@gmail.com>
2019-05-16 17:21:20 +0500

    change a name in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,7 +2,7 @@
 User.create({email: "badar@gmail.com",        password: "123456", password_confirmation: "123456", name: "Badar",        user_type: "manager"})
 User.create({email: "rehmat@gmail.com",       password: "123456", password_confirmation: "123456", name: "Rehmat",       user_type: "manager"})
 User.create({email: "junaid@gmail.com",       password: "123456", password_confirmation: "123456", name: "Junaid",       user_type: "qa"})
-User.create({email: "abdul.wahab@gmail.com",  password: "123456", password_confirmation: "123456", name: "Yasir",        user_type: "qa"})
+User.create({email: "abdul.wahab@gmail.com",  password: "123456", password_confirmation: "123456", name: "Abdul Wahab",        user_type: "qa"})
 User.create({email: "ali@gmail.com",          password: "123456", password_confirmation: "123456", name: "Ali",          user_type: "developer"})
 User.create({email: "muneeb@gmail.com",       password: "123456", password_confirmation: "123456", name: "Muneeb",       user_type: "developer"})
 User.create({email: "hashim.iqbal@gmail.com", password: "123456", password_confirmation: "123456", name: "Hashim Iqbal", user_type: "developer"})
