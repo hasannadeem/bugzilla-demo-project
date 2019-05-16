f7e2ec05f0805054179e92270b8bd437730cbe4a
MuneebDevs <muneebjavamob@gmail.com>
2019-05-16 16:46:46 +0500

    create seed for users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+User.create({email: "badar@gmail.com",        password: "123456", password_confirmation: "123456", name: "Badar",        user_type: "manager"})
+User.create({email: "rehmat@gmail.com",       password: "123456", password_confirmation: "123456", name: "Rehmat",       user_type: "manager"})
+User.create({email: "junaid@gmail.com",       password: "123456", password_confirmation: "123456", name: "Junaid",       user_type: "qa"})
+User.create({email: "abdul.wahab@gmail.com",  password: "123456", password_confirmation: "123456", name: "Yasir",        user_type: "qa"})
+User.create({email: "ali@gmail.com",          password: "123456", password_confirmation: "123456", name: "Ali",          user_type: "developer"})
+User.create({email: "muneeb@gmail.com",       password: "123456", password_confirmation: "123456", name: "Muneeb",       user_type: "developer"})
+User.create({email: "hashim.iqbal@gmail.com", password: "123456", password_confirmation: "123456", name: "Hashim Iqbal", user_type: "developer"})
+User.create({email: "hassan@gmail.com",       password: "123456", password_confirmation: "123456", name: "Hassan",       user_type: "developer"})
+User.create({email: "yasir@gmail.com",        password: "123456", password_confirmation: "123456", name: "Yasir",        user_type: "developer"})
+
