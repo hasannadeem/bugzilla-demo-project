2ab5f3739d11197fc3c7c61d6ed9e9fd3a175a5f
MuneebDevs <muneebjavamob@gmail.com>
2019-05-17 14:45:02 +0500

    add link to add user

--- app/presenters/project_presenter.rb
+++ app/presenters/project_presenter.rb
@@ -16,14 +16,16 @@ class ProjectPresenter < BasePresenter
 		@un_assigned_qas = User.where(user_type:'qa') - project_qas
 	end
 	
-
+	def link_to_add_user_to_project user_id
+		h.link_to("Add", h.add_user_project_path(user_id:user_id), :class =>"btn btn-outline-primary", method: :patch)
+	end
 
 	def link_to_delete_user_from_project user_id
-		h.link_to("delete", h.project_user_path(:user_id=>user_id,:project_id=>@project.id), :class =>"btn btn-light text-danger", data: { confirm: 'Are you sure?' }, method: :delete)
+		h.link_to("Delete", h.remove_user_project_path(user_id:user_id), :class =>"btn btn-light text-danger", data: { confirm: 'Are you sure?' }, method: :patch)
 	end
 
 	def link_to_delete_project
-		h.link_to "delete", @project, :class =>"btn btn-outline-danger",data: { confirm: 'Are you sure?' }, method: :delete
+		h.link_to "Delete", @project, :class =>"btn btn-outline-danger",data: { confirm: 'Are you sure?' }, method: :delete
 	end
 
 	def project_devs_count
